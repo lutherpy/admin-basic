6253e52a6de7ccb8e420fd33b3b6c883706173f8
lutherpy <lutheraccel@gmail.com>
2025-02-12 23:31:54 +0100

    CREATE & READ

--- app/api/assets/route.ts
+++ app/api/assets/route.ts
@@ -1,7 +1,8 @@
 import { NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+
+    const prisma = new PrismaClient();
 
 export async function GET() {
     try {
